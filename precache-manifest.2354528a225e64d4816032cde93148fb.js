5e639f192dfa91360da2c39215ce5881a53d6b18
niksein007 <markpearceniksein@gmail.com>
2020-05-24 18:54:28 +0100

    Updates

---
+++ precache-manifest.2354528a225e64d4816032cde93148fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5478d30f9a5a601569d699daed83a80b",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "c380367668bc27561320",
+    "url": "/memory-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "60b6fafbd3b172256184",
+    "url": "/memory-game/static/js/2.ef7ca72e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.ef7ca72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c380367668bc27561320",
+    "url": "/memory-game/static/js/main.4806e964.chunk.js"
+  },
+  {
+    "revision": "8e787906f2d28b5141ba",
+    "url": "/memory-game/static/js/runtime-main.bbb8fc2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
