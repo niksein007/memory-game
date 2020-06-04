ab8fedf30227d708868d35dd682c091b0d3fca5e
niksein007 <markpearceniksein@gmail.com>
2020-06-04 14:06:07 +0100

    project overhaul

--- src/App.js
+++ src/App.js
@@ -1,113 +1,33 @@
 import React from "react";
-import "./styles/App.css";
-
-import apple from "./components/images/apple.png";
-import cabbage from "./components/images/cabbage.png";
-import corn from "./components/images/corn.png";
-import cupCake from "./components/images/cup-cake.png";
-import mainBbg from "./components/images/main-bg.png";
-import popCorn from "./components/images/pop-corn.png";
-import cup from "./components/images/cup.png";
-import whiteBg from "./components/images/white-bg.png";
-// import images from'./components/Images';
 import ImageCard from "./components/ImageCard";
+import { imagesData } from "./components/imagesData";
+import "./styles/App.css";
 
 class App extends React.Component {
   state = {
-    cardsChosen: [],
-    cardsChosenId: [],
-    cardsWon: [],
-    images: [
-      { name: "apple", img: apple },
-      { name: "cabbage", img: cabbage },
-      { name: "corn", img: corn },
-      { name: "cupCake", img: cupCake },
-      { name: "popCorn", img: popCorn },
-      { name: "cup", img: cup },
-      { name: "apple", img: apple },
-      { name: "cabbage", img: cabbage },
-      { name: "corn", img: corn },
-      { name: "cupCake", img: cupCake },
-      { name: "popCorn", img: popCorn },
-      { name: "cup", img: cup },
-    ],
-  };
-  checkForMatch = () => {
-    let cards = document.querySelectorAll("img");
-    let resultDisplay = document.getElementById("result");
-
-    const firstPicId = this.state.cardsChosenId[0];
-    const secondPicId = this.state.cardsChosenId[1];
-    console.log(firstPicId);
-    console.log(secondPicId);
-
-    if (this.state.cardsChosen[0] === this.state.cardsChosen[1]) {
-      cards[firstPicId].setAttribute("src", whiteBg);
-      cards[secondPicId].setAttribute("src", whiteBg);
-      this.setState(
-        { cardsWon: [...this.state.cardsWon, this.state.cardsChosen] },
-        () => {}
-      );
-    } else {
-      // set back to normal
-      cards[firstPicId].setAttribute("src", mainBbg);
-      cards[secondPicId].setAttribute("src", mainBbg);
-      // reset pointer events
-      cards[firstPicId].style.pointerEvents = "auto";
-      cards[secondPicId].style.pointerEvents = "auto";
-    }
-    // set the two variables to empty
-    this.setState({ cardsChosen: [] });
-    this.setState({ cardsChosenId: [] });
-
-    // displaying results
-    resultDisplay.textContent = this.state.cardsWon.length;
-    if (this.state.cardsWon.length === this.state.images.length / 2) {
-      resultDisplay.textContent = "Congratulations You have Won!!!";
-    }
-  };
-
-  handleState = (name, id) => {
-    this.setState({ cardsChosenId: [...this.state.cardsChosenId, id] }, () => {
-      console.log(this.state.cardsChosenId);
-    });
-    this.setState({ cardsChosen: [...this.state.cardsChosen, name] }, () => {
-      console.log(this.state.cardsChosen);
-      if (this.state.cardsChosen.length === 2) {
-        console.log(this.state.cardsChosen);
-
-        setTimeout( this.checkForMatch(),500)
-      }
-    });
+    images: imagesData.images,
+    backgroundImages: imagesData.backgroungImages,
   };
 
   render() {
-    
+    // console.log(this.state.images);
 
-    const imageCard = this.state.images.map((value, key) => {
-      //  console.log(value.name);
-
-      return (
-        <ImageCard
-          handleState={this.handleState}
-          key={key}
-          id={key}
-          name={value.name}
-          img={value.img}
-        />
-
-
-      );
+    const imageGrid = this.state.images.map((image, index) => {
+      let duplicateImg = [];
+      for (let i = 0; i < 2; i++) {
+        duplicateImg = [
+          ...duplicateImg,
+          <ImageCard
+            key={index + i}
+            image={image}
+            backgroundImages={this.state.backgroundImages}
+          />,
+        ];
+      }
+      return duplicateImg;
     });
 
-    return (
-      <div className="App">
-        <h5>
-          Score:<span id="result"></span>
-        </h5>
-        <div className="grid">{imageCard}</div>
-      </div>
-    );
+    return <div className="App">{imageGrid}</div>;
   }
 }
 
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,33 +1,24 @@
 import React from "react";
 
-class ImageCard extends React.Component {
-  
+const ImageCard = (props) => {
+  // console.log(props);
+  const flipImage = (e)=>{
+    console.log(e.target)
+   let img = e.target
+    img.style.pointerEvents='none'
+    img.setAttribute('src',props.image.url)
+    img.setAttribute('alt',props.image.name)
 
-  imageFlip = (e) => {
- let image = e.target;
-    // let image = document.getElementById(this.props.id);
-
-    // disable the element after clicking
-    image.style.pointerEvents = "none";
-    image.setAttribute("src", this.props.img);
-    //calling the handleState method to reference and change the same state 
-    //if the state was in this component it will be referencing a new state 
-    //each time the image was clicked
-    this.props.handleState(this.props.name,this.props.id)
-
-
-   
-  };
-  render() {
-    return (
-      <img
-        className="ImageCard"
-        id={this.props.id}
-        onClick={this.imageFlip}
-        alt=""
-      />
-    );
   }
-}
+
+  return (
+    <img
+      className="ImageCard"
+      onClick={(e)=>flipImage(e)}
+      src={props.backgroundImages.mainBg.url}
+      alt={props.backgroundImages.mainBg.name}
+    />
+  );
+};
 
 export default ImageCard;
