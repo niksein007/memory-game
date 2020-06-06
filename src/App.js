016fa65b931357b27ed7986e1d147a175b3448f1
niksein007 <markpearceniksein@gmail.com>
2020-06-06 19:43:19 +0100

    more stable fixed life cycle errors

--- src/App.js
+++ src/App.js
@@ -5,60 +5,72 @@ import "./styles/App.css";
 
 class App extends React.Component {
   state = {
-    images: imagesData.images,
-    backgroundImages: imagesData.backgroungImages,
+    images: [],
+    backgroundImages: {},
+    score: 0,
+    imageHolder: [],
+    // showImage:false,
+  };
+  updateScore = () => {
+    this.setState((prevState) => {
+      return { score: prevState.score + 1 };
+    });
+  };
+  updateImageHolder = (index, url) => {
+    this.setState((prevState) => {
+      return {
+        //using destructing to pass in the parameters index and url
+        imageHolder: [...prevState.imageHolder, { index, url }],
+        // showImage:true,
+      };
+    });
   };
 
+  clearImageHolder = () => {
+    this.setState({
+      imageHolder: [],
+    });
+  };
+
+  componentDidMount() {
+    let imageDuplicte = [...imagesData.images, ...imagesData.images];
+    //sort the imageDuplicate in random order// not sorting after map because react will not
+    //know where the keys are when it re renders
+    imageDuplicte.sort(function (a, b) {
+      return 0.5 - Math.random();
+    });
+    this.setState({
+      images: imageDuplicte,
+      backgroundImages: imagesData.backgroungImages,
+    });
+  }
   render() {
-    let imageHolder = [];
-    //the img is the obj targeted using e.target in imageCard.js
-    const checkLogic = (img, url) => {
-      imageHolder = [...imageHolder, { url: url, img: img }];
-      console.log(imageHolder);
-      if (imageHolder.length === 2) {
-        if (imageHolder[0].url === imageHolder[1].url) {
-          imageHolder[0].img.setAttribute("src", this.state.backgroundImages.whiteBg.url);
-          imageHolder[0].img.setAttribute("alt", this.state.backgroundImages.whiteBg.name);
-
-          imageHolder[1].img.setAttribute("src", this.state.backgroundImages.whiteBg.url);
-          imageHolder[1].img.setAttribute("alt", this.state.backgroundImages.whiteBg.name);
-          
-        } else {
-          imageHolder[0].img.setAttribute("src", this.state.backgroundImages.mainBg.url);
-          imageHolder[0].img.setAttribute("alt", this.state.backgroundImages.mainBg.name);
-          imageHolder[0].img.style.pointerEvents = "auto";
-
-          imageHolder[1].img.setAttribute("src", this.state.backgroundImages.mainBg.url);
-          imageHolder[1].img.setAttribute("alt", this.state.backgroundImages.mainBg.name);
-          imageHolder[1].img.style.pointerEvents = "auto";
-        } 
-         // clear imageHolder
-      imageHolder=[]
-      }else{
-
-        }
-      
-      }
-    
-
-    //creates duplicate images of the images array
     const imageGrid = this.state.images.map((image, index) => {
-      let duplicateImg = [];
-      for (let i = 0; i < 2; i++) {
-        duplicateImg = [
-          ...duplicateImg,
-          <ImageCard
-            key={index + i}
-            image={image}
-            backgroundImages={this.state.backgroundImages}
-            checkLogic={checkLogic}
-          />,
-        ];
-      }
-      return duplicateImg;
+      return (
+        <ImageCard
+          key={index}
+          index={index}
+          image={image}
+          backgroundImages={this.state.backgroundImages}
+          updateImageHolder={this.updateImageHolder}
+          clearImageHolder={this.clearImageHolder}
+          imageHolder={this.state.imageHolder}
+          updateScore={this.updateScore}
+          // showImage={this.state.showImage}
+        />
+      );
     });
 
-    return <div className="App">{imageGrid}</div>;
+
+    return (
+      <div className="App">
+        <div>
+          <span>count:</span> <span>{this.state.score}</span>
+          <p>{this.state.score === 12 &&'Congratulation you Win!!!'}</p>
+        </div>
+        <div className="appImages">{imageGrid}</div>
+      </div>
+    );
   }
 }
 
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,25 +1,98 @@
 import React from "react";
 
-const ImageCard = (props) => {
-  const flipImage = (e) => {
-    // console.log(e.target)
-    let img = e.target;
-    img.style.pointerEvents = "none";
-    img.setAttribute("src", props.image.url);
-    img.setAttribute("alt", props.image.name);
-    //delaying the checkLogic funtion so that the image always shows Note:use an anonimous function
-    setTimeout(()=> props.checkLogic(img, props.image.url,),500,)
-    
+class ImageCard extends React.Component {
+  // using state to set src and alt enables the new states(alt/src)to be availble when rerender
+  // is done because setState was called
+  constructor(props) {
+    super(props);
+    // console.log("constructor");
 
+    this.state = {
+      src: this.props.image.url,
+      alt: this.props.image.name,
+      pointerEvents: "auto",
+      // imageHolder: this.props.imageHolder,
+      showImage: false,
+      backgroundImage: this.props.backgroundImages.mainBg.url,
+    };
+  }
+
+  flipImage = (e) => {
+    // console.log(e.target)
+    this.setState((prevState) => {
+      return { pointerEvents: "none", showImage: !prevState.showImage };
+    });
+    this.props.updateImageHolder(this.props.index, this.props.image.url);
   };
-  return (
-    <img
-      className="ImageCard"
-      onClick={(e) => flipImage(e)}
-      src={props.backgroundImages.mainBg.url}
-      alt={props.backgroundImages.mainBg.name}
-    />
-  );
-};
+
+  checkLogic = () => {
+    // console.log(this.props.imageHolder);
+    let imageHolder = this.props.imageHolder;
+    if (
+      imageHolder.length === 2 &&
+      imageHolder[0].url === imageHolder[1].url &&
+      this.props.image.url === imageHolder[0].url
+    ) {
+      console.log("yes");
+      this.setState((prevState) => {
+        return {
+          showImage: !prevState,
+          backgroundImage: this.props.backgroundImages.whiteBg.url,
+        };
+      });
+      this.props.clearImageHolder();
+      this.props.updateScore();
+    } else if (
+      imageHolder.length === 2 &&
+      imageHolder[0].url !== imageHolder[1].url
+    ) {
+      if (
+        this.props.index === imageHolder[0].index ||
+        this.props.index === imageHolder[1].index
+      ) {
+        console.log("not the same" + this.props.index+this.props.imageHolder );
+        this.setState((prevState) => {
+          return {
+            pointerEvents: "auto",
+            showImage: !prevState,
+          };
+        });
+      }
+
+      this.props.clearImageHolder();
+    }
+  };
+
+
+  componentDidUpdate() {
+    // let check = this.checkLogic();
+    console.log("did update");
+    // setTimeout(()=>{this.checkLogic()}, 100);
+    this.checkLogic();
+  }
+  componentDidMount() {
+    console.log("did mount");
+  }
+  render() {
+    return (
+      <div
+        onClick={this.flipImage}
+        className="ImageCard"
+        style={{
+          backgroundImage: `url(${this.state.backgroundImage})`,
+          pointerEvents: this.state.pointerEvents,
+        }}
+      >
+        {this.state.showImage ? (
+          <img
+            src={this.state.src}
+            alt={this.state.alt}
+            style={{ pointerEvents: this.state.pointerEvents }}
+          />
+        ) : undefined}
+      </div>
+    );
+  }
+}
 
 export default ImageCard;
